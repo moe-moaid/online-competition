037a7f77d607cb8c4ed6720a1db9ebd86b46222c
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-06 18:51:41 +0500

    added tailwind with shadcn, created header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <main>
+      <main className="bg-black">
+        <nav className="px-[150px] py-4"><Navbar/></nav>
       </main>
       <footer>
       </footer>
