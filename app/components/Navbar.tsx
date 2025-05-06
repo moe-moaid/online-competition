57429ac4d4e03abf7a0305ce61523e79da8e2b2e
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-06 18:58:03 +0500

    fixed image size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,9 @@ import { Button } from "@/components/ui/button";
 function Navbar() {
   return (
     <div className="flex flex-row items-center justify-between">
-        <div className="w-[250px] h-[65px]">
-            <Image src={"/logo.png"} width={250} height={65} alt="logo" />
-        </div>
+      <div className="w-[250px] h-[65px]">
+        <Image src={"/logo.png"} width={250} height={65} alt="logo" />
+      </div>
       <div className="flex flex-row items-center justify-between gap-x-14">
         <div className="flex text-white flex-row items-center justify-center gap-x-4">
           <Link href="/">Home</Link>
@@ -18,7 +18,7 @@ function Navbar() {
         </div>
         <div className="flex gap-x-8">
           <Button
-            className="bg-transparent border border-[#18AEBF] rounded-md text-white"
+            // className="bg-transparent border border-[#18AEBF] rounded-md text-white"
             variant="outline"
           >
             Login
