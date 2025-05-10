b52bb453e75365f327e9f0de294c9a97c597072c
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-10 23:56:13 +0500

    init container

---
+++ app/components/MciContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// this container applies the style of padding left and right along with other common styles so that it is easier to change if needed, instead of changing accross whole platform
+function MciContainer() {
+  return (
+    <div>MciContainer</div>
+  )
+}
+
+export default MciContainer
