964a44cfbefb1281b5e2a17c818040f3294cf58d
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-11 20:49:38 +0500

    more jobe done

--- app/components/MciContainer.tsx
+++ app/components/MciContainer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 // this container applies the style of padding left and right along with other common styles so that it is easier to change if needed, instead of changing accross whole platform
-function MciContainer() {
+function MciContainer({ children }: Readonly<{children: React.ReactNode;}>) {
   return (
-    <div>MciContainer</div>
+      <div className={`px-3 lg:px-[90px] xl:px-[150px]`}>
+          {children}
+    </div>
   )
 }
 
