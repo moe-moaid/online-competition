ac5d30587a77e4fdc3dc00f91c78d196dedc7f7c
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-14 00:02:57 +0500

    stoped hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main>
+        <div>
+          <p>something here to prevent hydration error</p>
+        </div>
+      </main>
     </div>
   );
 }
