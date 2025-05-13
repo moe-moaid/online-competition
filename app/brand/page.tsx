34ee6c5c29e1ad813827db319969d63c7db339ac
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-14 00:40:39 +0500

    page inint

---
+++ app/brand/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MciContainer from '../components/MciContainer'
+
+function page() {
+  return (
+      <MciContainer>
+        <div className=''></div>
+    </MciContainer>
+  )
+}
+
+export default page
