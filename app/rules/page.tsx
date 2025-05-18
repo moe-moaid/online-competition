32b34c6d86731f3ae54f5eb768d4505009f8d94b
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-18 23:48:04 +0500

    added title

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MciContainer from '../components/MciContainer'
 
 function page() {
   return (
-    <div className='text-white'>welcome to rules page</div>
+      <MciContainer>
+          <div className="flex flex-col items-center text-white">
+              <h1 className='font-bold text-[40px]'>Rules of <span className='text-legendary-500'>Mackie</span></h1>
+              <p>Rules are simple, so simple you can't mess up, listed below</p>
+          </div>
+    </MciContainer>
   )
 }
 
