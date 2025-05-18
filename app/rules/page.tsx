43f8926cf9a1f79f57bf2d73cd48946e4af8b042
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-18 23:55:52 +0500

    established rules

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import MciContainer from '../components/MciContainer'
+import React from "react";
+import MciContainer from "../components/MciContainer";
 
 function page() {
   return (
-      <MciContainer>
-          <div className="flex flex-col items-center text-white">
-              <h1 className='font-bold text-[40px]'>Rules of <span className='text-legendary-500'>Mackie</span></h1>
-              <p>Rules are simple, so simple you can't mess up, listed below</p>
+    <MciContainer>
+      <div className="flex flex-col items-center text-white">
+        <h1 className="font-bold text-[40px]">
+          Rules of <span className="text-legendary-500">Mackie</span>
+        </h1>
+        <p>Rules are simple, so simple you can't mess up, listed below</p>
+      </div>
+      <div className="flex flex-row flex-wrap text-white">
+              <div className="flex flex-row jsutify-start items-baseline">
+          <p className="font-bold text-[120px] text-white/20">01</p>
+          <div>
+            <h4 className="font-bold text-[20px]">Upload Video</h4>
+            <p>Each participant is allowed to upload 1 video.</p>
           </div>
+        </div>
+      </div>
     </MciContainer>
-  )
+  );
 }
 
-export default page
+export default page;
