c19ca011839301883777b74ba0fbc39af3ba8e69
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-19 00:20:08 +0500

    added rules

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import MciContainer from "../components/MciContainer";
 
+const Rules = [
+  {
+    number: "01",
+    title: "Upload Video",
+    desc: "Each participant is allowed to upload 1 video.",
+  },
+  {
+    number: "02",
+    title: "Unique ID",
+    desc: "Each participant will receive a unique ID upon creation of the Account.",
+  },
+  {
+    number: "03",
+    title: "Multiple IDs",
+    desc: "If the Participant creates multiple IDs, only the first video will be taken into consideration.",
+  },
+  {
+    number: "04",
+    title: "Final Stage",
+    desc: "The Final will feature only the first 10 highest voted participants",
+  },
+  {
+    number: "05",
+    title: "After Final",
+    desc: "After the Final, the winner will be contacted by our team within 48 hours to be informed about the next steps!",
+  },
+];
 function page() {
   return (
     <MciContainer>
@@ -10,14 +37,18 @@ function page() {
         </h1>
         <p>Rules are simple, so simple you can't mess up, listed below</p>
       </div>
-      <div className="flex flex-row flex-wrap text-white">
-              <div className="flex flex-row jsutify-start items-baseline">
-          <p className="font-bold text-[120px] text-white/20">01</p>
-          <div>
-            <h4 className="font-bold text-[20px]">Upload Video</h4>
-            <p>Each participant is allowed to upload 1 video.</p>
+      <div className="flex flex-row flex-wrap text-white justify-center">
+        {Rules.map((rule, i) => (
+          <div key={`${rule.number} - ${i}`} className="flex flex-row jsutify-start w-1/3 items-baseline">
+            <p className="font-bold text-[120px] text-white/20 inline-block">
+              {rule.number}
+            </p>
+            <div>
+              <h4 className="font-bold text-[20px]">{rule.title}</h4>
+              <p>{rule.desc}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </MciContainer>
   );
