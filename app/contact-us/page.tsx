7fc77627da631f7a893d2963dd45baff7be75303
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-20 17:36:50 +0500

    page init

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MciContainer from '../components/MciContainer'
+
+function page() {
+  return (
+    <MciContainer>
+        <p className='text-white'>welcome to contact us page</p>
+    </MciContainer>
+  )
+}
+
+export default page
