bc8d2a89d3625c8f7e184133082743ea88fd6200
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-20 16:00:58 +0500

    page init, accordion added

---
+++ app/faq/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import MciContainer from '../components/MciContainer'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+
+function page() {
+  return (
+    <MciContainer className='py-[120px] text-white'>
+        <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </MciContainer>
+  )
+}
+
+export default page
