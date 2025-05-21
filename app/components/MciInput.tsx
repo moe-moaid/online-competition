20dd30dc865292ee67daaeff2fdde8e565399120
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-21 18:49:54 +0500

    done

---
+++ app/components/MciInput.tsx
@@ -0,0 +1,35 @@
+import { Span } from "next/dist/trace";
+import React from "react";
+
+type inputProps = {
+  className?: string;
+  required?: boolean;
+  label?: string;
+  placeholder: string;
+  name: string;
+fullWidth?: boolean;
+};
+function MciInput({
+  className,
+  name,
+  required,
+  label,
+  placeholder,
+  fullWidth
+}: inputProps) {
+  return (
+    <div className={`flex flex-col gap-y-3 ${fullWidth ? 'w-full' : ''} ${className}`}>
+      {/* <label htmlFor={name}>{`${label}`}</label> */}
+      <label htmlFor={name}>{label ? label : ''}{required && <span className="text-red-600">*</span>}</label>
+      <input
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        className="outline-none border border-white rounded-md px-3 py-1 bg-transparent text-[#838383] hover:border-legendary-500 hover:text-white focus:border-legendary-500 focus:text-white"
+        required={required ? true : false}
+      />
+    </div>
+  );
+}
+
+export default MciInput;
---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import MciContainer from "../components/MciContainer";
+import MciInput from "../components/MciInput";
+import { Button } from "@/components/ui/button";
+
+function page() {
+  return (
+    <MciContainer className="text-white h-full py-[120px] bg-cover bg-center bg-no-repeat bg-[url('/how-it-works.png')] relative z-0">
+        <div className="bg-black/85 absolute inset-0 -z-10" />
+      <div className="w-full md:w-4/5 lg:w-3/5 xl:w-2/5 mx-auto bg-gray-bg px-2 sm:px-10 md:px-14 lg:px-24 py-12 rounded-lg">
+        <h1 className="font-semibold text-[48px] text-center w-full mb-10">
+          log<span className="text-legendary-500">in</span>
+        </h1>
+        <form className="flex flex-col gap-y-6 justify-center items-center w-full">
+          {/*  className, name, required, label, placeholder  */}
+          <MciInput
+            name="email"
+            required
+            label="Email"
+            placeholder="Email"
+            fullWidth
+          />
+          <MciInput
+            name="password"
+            required
+            label="Password"
+            placeholder="Password"
+            fullWidth
+          />
+          <Button className="py-6 bg-legendary-500 w-full" variant='default' size='lg'>login</Button>
+        </form>
+      </div>
+    </MciContainer>
+  );
+}
+
+export default page;
