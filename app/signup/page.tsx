79600134f1f3613b28b1a25dedf0338ece38f375
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-05-23 15:52:25 +0500

    ready MVP

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import MciContainer from '../components/MciContainer'
+import MciInput from '../components/MciInput'
 
 function page() {
   return (
-    <div className='text-white'>signup page</div>
+    <div className='py-[120px] flex flex-row justify-center items-center'>
+        <form action="" className='flex flex-col items-center md:w-2/3 lg-1/2 justify-center px-10 py-12 bg-gray-bg rounded-lg gap-y-8'>
+            <h1 className='text-white'>Sign<span className='text-legendary-500'>up</span></h1>
+            <div className="flex flex-col sm:flex-row justify-between items-center w-full gap-x-8">
+                <MciInput name='name' label='Full Name' placeholder='Full Name' required fullWidth/>
+                <MciInput name='age' label='Age' placeholder='Age' fullWidth/>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-between items-center w-full gap-x-8">
+                <MciInput name='email' label='Email' placeholder='Email' required fullWidth/>
+                <MciInput name='phoneNumer' label='Phone Numer' placeholder='Phone Numer' required fullWidth/>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-between items-center w-full gap-x-8">
+                <MciInput name='password' label='Password' placeholder='Password' required fullWidth/>
+                <MciInput name='confirmPass' label='Confirm Passowrd' placeholder='Confirm Passowrd' required fullWidth/>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-between items-center w-full gap-x-8">
+                <MciInput name='country' label='Country' placeholder='Country' fullWidth/>
+                <MciInput name='address' label='Address' placeholder='Address' fullWidth/>
+            </div>
+        </form>
+
+    </div>
   )
 }
 
