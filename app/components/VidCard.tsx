187d44e7aef192cf550c4236b0ac8678a2b90db9
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-29 23:46:57 +0500

    gradually completing

--- app/components/VidCard.tsx
+++ app/components/VidCard.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function VidCard() {
+type Props = {
+  country: string;
+  title: string;
+  artist: string;
+  isVerified: boolean;
+  imageUrl: string;
+};
+function VidCard({ country, title, artist, isVerified, imageUrl }: Props) {
   return (
-    <div>VidCard</div>
-  )
+    <div className="p-5">
+      <div className="relative">
+        <p className="absolute top-0 left-1/2 -translate-x-1/2 px-2 py-1 rounded-bl-lg rounded-br-lg bg-legendary-500 font-semibold text-[20px]">
+          {country}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default VidCard
+export default VidCard;
