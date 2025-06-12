2af2c2ba1fd7b134f4ae7d30465be82db39b68ca
Moe_Moaid <mohammadmoaid@gmail.com>
2025-06-12 23:57:28 +0500

    init form

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,19 @@
 import { useUploadVideo } from "@/lib/uploadService";
 
 export default function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <p>something here to prevent hydration error</p>
+          <VideoUploadingForm />
+        </div>
+      </main>
+    </div>
+  );
+}
 
+function VideoUploadingForm() {
   const { mutate, data, error, isPending } = useUploadVideo();
   const handleVideoUpload = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,32 +26,30 @@ export default function Home() {
 
     const uploadFormData = new FormData();
     uploadFormData.append("video", file);
+    uploadFormData.append("title", "My Cool Video");
+    uploadFormData.append("description", "Optional description");
+    uploadFormData.append("artistId", "3");
 
-    console.log("file name ===", file, uploadFormData, typeof uploadFormData);
     for (let [key, value] of uploadFormData.entries()) {
-      console.log('form entries', key, value);
+      console.log("form entries", key, value);
     }
     mutate({
       data: uploadFormData,
     });
   };
   return (
-    <div>
-      <main>
-        <div>
-          <p>something here to prevent hydration error</p>
-          <form onSubmit={handleVideoUpload}>
-            <input type="file" name="video" />
-            <button type="submit" disabled={isPending}>
-              <p className="text-white">
-                {isPending ? "Uploading..." : "Upload"}
-              </p>
-            </button>
-            {error && <p className="text-amber-500">Error uploading video</p>}
-            {data && <p className="text-lime-500">Upload successful!</p>}
-          </form>
-        </div>
-      </main>
-    </div>
+    <form onSubmit={handleVideoUpload}>
+      <input type="file" name="video" />
+      <button type="submit" disabled={isPending}>
+        <p className="text-white">{isPending ? "Uploading..." : "Upload"}</p>
+      </button>
+
+      <input type="text" name="discription" />
+      <textarea cols={5} rows={7} />
+      <input type="text" name="title" />
+      <input type="text" name="gener" />
+      {error && <p className="text-amber-500">Error uploading video</p>}
+      {data && <p className="text-lime-500">Upload successful!</p>}
+    </form>
   );
 }
