699e466a0a95403bb78b0ba74d84153022c7c071
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-07-10 21:44:58 +0500

    displayed main video

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -1,8 +1,17 @@
+'use client';
 import React from 'react'
+import { useGetListVideos } from '@/lib/getListService';
 
 function Vote() {
+  const { data: videos, error: vid_error } = useGetListVideos();
   return (
-    <p>Vote page</p>
+    <div>
+        {/* <section className=''> */}
+            <div className="relative w-full h-[580px]">
+                <video className='w-full h-[580px] object-cover' src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}/>
+            </div>
+        {/* </section> */}
+    </div>
   )
 }
 
