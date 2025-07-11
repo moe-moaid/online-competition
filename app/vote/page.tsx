ac4430db54e6ce88a423709f9a613cbcc38e243b
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-07-11 18:21:28 +0500

    added the side component for scrollable vidz

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -1,18 +1,27 @@
-'use client';
-import React from 'react'
-import { useGetListVideos } from '@/lib/getListService';
+"use client";
+import React from "react";
+import { useGetListVideos } from "@/lib/getListService";
+import { videoType } from "@/lib/types/videoType";
 
 function Vote() {
   const { data: videos, error: vid_error } = useGetListVideos();
   return (
-    <div>
-        {/* <section className=''> */}
-            <div className="relative w-full h-[580px]">
-                <video className='w-full h-[580px] object-cover' src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}/>
-            </div>
-        {/* </section> */}
-    </div>
-  )
+    <>
+      <section className="h-[580px]">
+        <video
+          className="w-full h-full object-cover"
+          src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}
+          controls
+        />
+        <div className="absolute h-full overflow-y-auto start-[150px] flex flex-col gap-y-5 items-start text-white">
+          <h1>Videos</h1>
+          {videos.map((video: videoType, i: number) => {
+            return <div></div>;
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Vote
+export default Vote;
