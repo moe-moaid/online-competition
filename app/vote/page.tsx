7060ee45ccef7a6dc6553be9a09dcdf7b170f606
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-07-13 21:46:26 +0500

    styled side videos

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useGetListVideos } from "@/lib/getListService";
 import { videoType } from "@/lib/types/videoType";
+import Image from "next/image";
 
 function Vote() {
   const { data: videos, error: vid_error } = useGetListVideos();
@@ -11,7 +12,6 @@ function Vote() {
         <video
           className="w-full h-full object-cover"
           src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}
-          controls
         />
         <div className="absolute h-full overflow-y-auto start-[150px] top-0 flex flex-col gap-y-2 items-start text-white">
           <h1>Videos</h1>
@@ -19,12 +19,26 @@ function Vote() {
             return (
               <div key={`${video.title} - ${i}`}>
                 <video
-                  className="w-[320px] h-[250px] rounded-sm"
+                  className="w-[320px] rounded-sm"
                   src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
-                  controls
                 />
+                <div className="flex flex-row justify-start items-center gap-x-4">
+
+<div className="relative w-10 h-10 rounded-full overflow-clip">
+<Image
+              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
+              fill
+              alt="singer image"
+              loading="lazy"
+            />
+</div>
+
+<div className="">
                 <p>{video.title}</p>
-                <p>{video.artist.name}</p>
+                <p>{`By ${video.artist.name}`}</p>
+</div>
+                </div>
+
               </div>
             );
           })}
