407164d35004a6849d1420a6d6b5af33777d7984
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-07-14 23:50:38 +0500

    more styling and forging

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -9,36 +9,35 @@ function Vote() {
   return (
     <>
       <section className="h-[580px] relative">
+        <div className="absolute bg-black/70 top-0 bottom-0 start-0 end-0" />
         <video
           className="w-full h-full object-cover"
           src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}
         />
-        <div className="absolute h-full overflow-y-auto start-[150px] top-0 flex flex-col gap-y-2 items-start text-white">
+        <div className="absolute h-full overflow-y-auto start-[150px] top-0 flex flex-col gap-y-4 items-start text-white pe-10 border-e border-white">
           <h1>Videos</h1>
           {videos?.map((video: videoType, i: number) => {
             return (
               <div key={`${video.title} - ${i}`}>
                 <video
-                  className="w-[320px] rounded-sm"
+                  className="w-[270px] rounded-sm"
                   src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
                 />
                 <div className="flex flex-row justify-start items-center gap-x-4">
+                  <div className="relative w-10 h-10 rounded-full overflow-clip">
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
+                      fill
+                      alt="singer image"
+                      loading="lazy"
+                    />
+                  </div>
 
-<div className="relative w-10 h-10 rounded-full overflow-clip">
-<Image
-              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
-              fill
-              alt="singer image"
-              loading="lazy"
-            />
-</div>
-
-<div className="">
-                <p>{video.title}</p>
-                <p>{`By ${video.artist.name}`}</p>
-</div>
+                  <div className="">
+                    <p>{video.title}</p>
+                    <p>{`By ${video.artist.name}`}</p>
+                  </div>
                 </div>
-
               </div>
             );
           })}
