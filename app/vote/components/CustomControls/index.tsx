a05e7ad6d8506b9bcaed5ce085a0a14a4a79a4b7
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-07-25 16:37:58 +0500

    done with play,pause, seek buttons

--- app/vote/components/CustomControls/index.tsx
+++ app/vote/components/CustomControls/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PlayPauseButton from "./PlayPauseButton";
+import SoundControl from "./SoundControl";
 
 type Props = {
   currentVideo: HTMLVideoElement | null;
@@ -11,17 +12,29 @@ function CustomControls({
   isVideoPlaying,
   setIsVideoPlaying,
 }: Props) {
+  function skip(value: number) {
+    if (currentVideo) currentVideo.currentTime += value;
+  }
+
   return (
-    <div>
-      {/* play pose button  ▶️⏸️ */}
-      <PlayPauseButton
-        currentVideo={currentVideo}
-        isVideoPlaying={isVideoPlaying}
-        setIsVideoPlaying={setIsVideoPlaying}
-      />
-      {/* Seek Buttons  ⏪️⏩️*/}
-      {/* Volume control */}
-      {/* Full screen toggle */}
+    <div className="flex flex-col w-full">
+      {/* progress bar */}
+
+      <div className="z-10 flex flex-row justify-between items-center">
+        {/* Volume control */}
+        {/* <SoundControl /> */}
+
+        {/* <div> */}
+          {/* play, pose, and seek buttons  ▶️⏸️ ⏪️⏩️ */}
+          <PlayPauseButton
+            currentVideo={currentVideo}
+            isVideoPlaying={isVideoPlaying}
+            setIsVideoPlaying={setIsVideoPlaying}
+          />
+        {/* </div> */}
+
+        {/* Full screen toggle */}
+      </div>
     </div>
   );
 }
--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -10,18 +10,18 @@ function Vote() {
   const [isVideoPlaying, setIsVideoPlaying] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   function playVideo() {
-    if (videoRef.current) {
+    if (videoRef.current && videoRef.current.paused) {
       videoRef.current.play();
       setIsVideoPlaying(true);
-    }
+    } 
   }
+
   return (
     <>
       <section className="h-[580px] relative">
         {!isVideoPlaying && (
           <div className="absolute bg-black/70 top-0 bottom-0 start-0 end-0 z-0" />
         )}
-<CustomControls currentVideo={videoRef.current} isVideoPlaying={isVideoPlaying} setIsVideoPlaying={setIsVideoPlaying}/>
         <video
           ref={videoRef}
           className="w-full h-full object-cover absolute -z-10"
@@ -40,7 +40,8 @@ function Vote() {
                         className="w-[270px] rounded-sm"
                         src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
                       />
-                      <div className="flex flex-row justify-start items-center gap-x-4">
+                      <div className="flex flex-row justify-between items-center">
+                        <div className="flex flex-row justify-start items-center gap-x-4">
                         <div className="relative w-10 h-10 rounded-full overflow-clip">
                           <Image
                             src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
@@ -49,11 +50,11 @@ function Vote() {
                             loading="lazy"
                           />
                         </div>
-
                         <div className="">
                           <p>{video.title}</p>
                           <p>{`By ${video.artist.name}`}</p>
                         </div>
+                      </div>
                       </div>
                     </div>
                   );
@@ -89,6 +90,52 @@ function Vote() {
                   </svg>
                 </span>
               </button>
+<CustomControls
+                currentVideo={videoRef.current}
+                isVideoPlaying={isVideoPlaying}
+                setIsVideoPlaying={setIsVideoPlaying}
+              />
+            </div>
+          </div>
+        )}
+        {isVideoPlaying && (
+          <div className="flex flex-row justify-start h-full ms-[160px]">
+            <div className="h-full overflow-y-auto start-[150px] top-0 flex flex-col gap-y-4 items-start text-white">
+              <div className="z-10 pe-10 border-e border-white">
+                <h1 className="z-10">Videos</h1>
+                {videos?.map((video: videoType, i: number) => {
+                  return (
+                    <div key={`${video.title} - ${i}`} className="mb-4">
+                      <video
+                        className="w-[270px] rounded-sm"
+                        src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
+                      />
+                      <div className="flex flex-row justify-start items-center gap-x-4">
+                        <div className="relative w-10 h-10 rounded-full overflow-clip">
+                          <Image
+                            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
+                            fill
+                            alt="singer image"
+                            loading="lazy"
+                          />
+                        </div>
+
+                        <div className="">
+                          <p>{video.title}</p>
+                          <p>{`By ${video.artist.name}`}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="text-white flex flex-col justify-end ps-10 pb-20 z-10 w-full">
+              <CustomControls
+                currentVideo={videoRef.current}
+                isVideoPlaying={isVideoPlaying}
+                setIsVideoPlaying={setIsVideoPlaying}
+              />
             </div>
           </div>
         )}
