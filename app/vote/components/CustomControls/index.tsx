bef6700e019f86c143eb7b5ad684f16e18ab9b38
Moe_Moaid <mohammadmoaid@gmail.com>
2025-07-26 23:43:35 +0500

    full screen MVP

--- app/vote/components/CustomControls/index.tsx
+++ app/vote/components/CustomControls/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlayPauseButton from "./PlayPauseButton";
 import SoundControl from "./SoundControl";
 
@@ -12,10 +12,31 @@ function CustomControls({
   isVideoPlaying,
   setIsVideoPlaying,
 }: Props) {
-  function skip(value: number) {
-    if (currentVideo) currentVideo.currentTime += value;
+  const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
+  function handleFullscreen() {
+    if (!document.fullscreenElement) {
+      if (currentVideo?.requestFullscreen) {
+        currentVideo.requestFullscreen();
+        setIsFullscreen(true);
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+        setIsFullscreen(false);
+      }
+    }
   }
 
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      const isFull = !!document.fullscreenElement;
+      setIsFullscreen(isFull);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    return () =>
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+  }, []);
   return (
     <div className="flex flex-col w-full">
       {/* progress bar */}
@@ -24,16 +45,32 @@ function CustomControls({
         {/* Volume control */}
         <SoundControl currentVideo={currentVideo} />
 
-        {/* <div> */}
-          {/* play, pose, and seek buttons  ▶️⏸️ ⏪️⏩️ */}
-          <PlayPauseButton
-            currentVideo={currentVideo}
-            isVideoPlaying={isVideoPlaying}
-            setIsVideoPlaying={setIsVideoPlaying}
-          />
-        {/* </div> */}
+        {/* play, pose, and seek buttons  ▶️⏸️ ⏪️⏩️ */}
+        <PlayPauseButton
+          currentVideo={currentVideo}
+          isVideoPlaying={isVideoPlaying}
+          setIsVideoPlaying={setIsVideoPlaying}
+        />
 
         {/* Full screen toggle */}
+        <button onClick={handleFullscreen}>
+          <svg
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.17181 12.4998L3.34206 18.5708L0 15.0875V24.9998H9.51581L6.17181 21.5164L12 15.4458L9.17181 12.4998Z"
+              fill="white"
+            />
+            <path
+              d="M14.4842 0L17.8282 3.48333L12 9.55397L14.8282 12.5L20.6579 6.42897L24 9.9123V0H14.4842Z"
+              fill="white"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
