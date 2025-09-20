75de3ec71efbc1a8e1260fab95d40470dd5cdeb8
Moe_Moaid <mohammadmoaid@gmail.com>
2025-09-20 23:53:48 +0500

    edited test

--- app/components/VidCard.test.tsx
+++ app/components/VidCard.test.tsx
@@ -30,24 +30,11 @@ describe("test video cards for rendering the right things", () => {
       </VoteProvider>
     );
   });
-//   queryBy, getBy, findB
-{/**
-    | Variant          | Return type                            | Throws if none found?       | Throws if >1 found?      |
-| ---------------- | -------------------------------------- | --------------------------- | ------------------------ |
-| **getBy\***      | Single element                         | ✅ Yes (if none)             | ✅ Yes (if more than one) |
-| **getAllBy\***   | Array of elements                      | ✅ Yes (if none)             | ❌ No (returns all)       |
-| **queryBy\***    | Single element or `null`               | ❌ No (returns null)         | ✅ Yes (if more than one) |
-| **queryAllBy\*** | Array of elements (empty if none)      | ❌ No (empty array)          | ❌ No                     |
-| **findBy\***     | Promise resolving to element           | ✅ Yes after timeout         | ✅ Yes (if more than one) |
-| **findAllBy\***  | Promise resolving to array of elements | ✅ Yes after timeout if none | ❌ No (returns all)       |
-
-*/}
 
   it("tests if the card has all the necessary components", () => {
-    // expect(screen.queryByText(/video for competing/i)).toBeInTheDocument(); // video name
-    // expect(screen.queryByText(/by jake/i)).toBeInTheDocument(); // artist name
-    // expect(screen.queryByRole('img')).toBeInTheDocument(); // artist avatar
-    // expect(screen.queryAllByRole('button')).toBeDefined(); // play button and vote button
-    expect(screen.queryByText(/hello/i)).toBeNull(); // artist avatar
+    expect(screen.queryByText(/video for competing/i)).toBeInTheDocument(); // video name
+    expect(screen.queryByText(/by jake/i)).toBeInTheDocument(); // artist name
+    expect(screen.queryByRole('img')).toBeInTheDocument(); // artist avatar
+    expect(screen.queryAllByRole('button')).toBeDefined(); // play button and vote button
   });
 });
