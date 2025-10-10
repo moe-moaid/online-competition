7817e5788837b3f6d908f3546c226964545c543e
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-10-10 13:51:14 +0500

    payment with vote working

---
+++ app/components/StripeWrapper.tsx
@@ -0,0 +1,18 @@
+import { useVoteContext } from "@/lib/context/vote context";
+import { PropsWithChildren } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+function StripeWrapper({ children }: PropsWithChildren) {
+  const { clientSecret } = useVoteContext();
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
+  return (
+    <Elements stripe={stripePromise} options={{ clientSecret }}>
+      {children}
+    </Elements>
+  );
+}
+
+export default StripeWrapper;
--- lib/services/vote.ts
+++ lib/services/vote.ts
@@ -1,7 +1,17 @@
 // services/votes.ts
 import { gqlRequest } from '@/lib/graphql/client/graphqlClient';
 import { CAST_VOTE } from '@/lib/graphql/mutations/mutations';
+import { CREATE_PAYMENT_INTENT } from '@/lib/graphql/mutations/mutations'
 
+type CreatePaymentIntentResponse = {
+  createPaymentIntent: string;
+};
+
+export async function createPaymentIntent(videoId: number) {
+  return gqlRequest<CreatePaymentIntentResponse>(CREATE_PAYMENT_INTENT, {
+    videoId,
+  });
+}
 export async function castVote(videoId: number) {
   return gqlRequest<{ castVote: { id: number; videoId: number } }>(CAST_VOTE, {
     videoId,
