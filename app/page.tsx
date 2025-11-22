269f9316b8ec10342a0d783274423bbc9a556b18
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-11-22 07:18:40 +0500

    made stripe wrapper global

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import Image from 'next/image';
 import PlayVideo from './components/PlayVideo';
 import CArdsFilterView from './components/CardsFilterView';
 import { useVoteContext } from '@/lib/context/vote context';
-import PaymentForm from './components/PaymentForm';
-import StripeWrapper from './components/StripeWrapper';
 
 export default function Home() {
   const { data: videos } = useGetListVideos();
@@ -35,110 +33,105 @@ export default function Home() {
   }
 
   return (
-    <StripeWrapper>
-      <PaymentForm />
-      <main className={`relative ${isVoteOpen && 'h-screen overflow-hidden'}`}>
-        {isVoteOpen && (
-          <>
-            <div
-              className="absolute bottom-0 left-0 right-0 top-0 z-20 bg-black/20 backdrop-blur-sm transition-transform duration-1000 ease-in-out hover:cursor-pointer"
-              onClick={() => setIsVoteOpen(false)}
-            />
-          </>
-        )}
-        <section className="w-full">
-          <div className="relative h-[350px] sm:h-[550] md:h-[750px] lg:h-[980px]">
-            <div className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-black/45" />
-            <Image src="/hero.jpg" fill alt="hero" />
-            <div className="w-max-full absolute start-[12px] top-[50%] z-10 -translate-y-[50%] transform tracking-widest text-white md:w-1/2 lg:start-[90px] xl:start-[150px]">
-              <h1 className="text-[18px] font-semibold lg:text-[40px] xl:text-[72px]">
-                All <span className="text-legendary-500">Abroad</span>
-              </h1>
-              <p className="sm:text[16px] text-[12px] font-medium lg:text-[18px] xl:text-[24px]">
-                Are you ready to be the next International Talent?
-              </p>
-              <p className="sm:text[16px] text-[12px] font-medium lg:text-[18px] xl:text-[24px]">
-                Do you think you have what it takes?
-              </p>
-              <p className="sm:text[16px] mt-4 text-[12px] font-medium lg:mt-9 lg:text-[18px] xl:text-[24px]">
-                Upload your video today and take part in our competition for a
-                chance to win and be featured with an international Artist.
-              </p>
-              <div className="sm:text[16px] mt-6 flex flex-row justify-start gap-x-4 text-[12px] font-medium lg:text-[18px] xl:text-[20px]">
-                <button className="sm-px-4  rounded-md bg-legendary-500 px-2 py-3 lg:px-6">
-                  Learn More
-                </button>
-                <button className="sm-px-4 rounded-md border border-legendary-500 bg-transparent px-2 py-2 text-legendary-500 lg:px-6">
-                  Watch Trailer
-                </button>
-              </div>
+    <main className={`relative ${isVoteOpen && 'h-screen overflow-hidden'}`}>
+      {isVoteOpen && (
+        <>
+          <div
+            className="absolute bottom-0 left-0 right-0 top-0 z-20 bg-black/20 backdrop-blur-sm transition-transform duration-1000 ease-in-out hover:cursor-pointer"
+            onClick={() => setIsVoteOpen(false)}
+          />
+        </>
+      )}
+      <section className="w-full">
+        <div className="relative h-[350px] sm:h-[550] md:h-[750px] lg:h-[980px]">
+          <div className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-black/45" />
+          <Image src="/hero.jpg" fill alt="hero" />
+          <div className="w-max-full absolute start-[12px] top-[50%] z-10 -translate-y-[50%] transform tracking-widest text-white md:w-1/2 lg:start-[90px] xl:start-[150px]">
+            <h1 className="text-[18px] font-semibold lg:text-[40px] xl:text-[72px]">
+              All <span className="text-legendary-500">Abroad</span>
+            </h1>
+            <p className="sm:text[16px] text-[12px] font-medium lg:text-[18px] xl:text-[24px]">
+              Are you ready to be the next International Talent?
+            </p>
+            <p className="sm:text[16px] text-[12px] font-medium lg:text-[18px] xl:text-[24px]">
+              Do you think you have what it takes?
+            </p>
+            <p className="sm:text[16px] mt-4 text-[12px] font-medium lg:mt-9 lg:text-[18px] xl:text-[24px]">
+              Upload your video today and take part in our competition for a
+              chance to win and be featured with an international Artist.
+            </p>
+            <div className="sm:text[16px] mt-6 flex flex-row justify-start gap-x-4 text-[12px] font-medium lg:text-[18px] xl:text-[20px]">
+              <button className="sm-px-4  rounded-md bg-legendary-500 px-2 py-3 lg:px-6">
+                Learn More
+              </button>
+              <button className="sm-px-4 rounded-md border border-legendary-500 bg-transparent px-2 py-2 text-legendary-500 lg:px-6">
+                Watch Trailer
+              </button>
             </div>
           </div>
-        </section>
-        <CArdsFilterView
-          showFilters={isChangeVoteOpen}
-          setDisplayVid={setDisplayVid}
-        />
-        {displayVid && (
-          <PlayVideo video={displayVid} setDisplayVid={setDisplayVid} />
-        )}
-        <section>
-          <MciContainer className="mt-14 bg-gray-bg py-32">
-            <h1 className="mb-14 text-center text-[40px] font-semibold text-white">
-              Featured <span className="text-legendary-500">Video</span>
-            </h1>
-            <div className="flex flex-col items-center justify-between gap-y-4 md:flex-row md:gap-x-4">
-              <div className="relative w-full md:w-1/2">
-                <button
-                  className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-125 transform duration-300 ease-in-out"
-                  onClick={handlePlayPause}
+        </div>
+      </section>
+      <CArdsFilterView
+        showFilters={isChangeVoteOpen}
+        setDisplayVid={setDisplayVid}
+      />
+      {displayVid && (
+        <PlayVideo video={displayVid} setDisplayVid={setDisplayVid} />
+      )}
+      <section>
+        <MciContainer className="mt-14 bg-gray-bg py-32">
+          <h1 className="mb-14 text-center text-[40px] font-semibold text-white">
+            Featured <span className="text-legendary-500">Video</span>
+          </h1>
+          <div className="flex flex-col items-center justify-between gap-y-4 md:flex-row md:gap-x-4">
+            <div className="relative w-full md:w-1/2">
+              <button
+                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-125 transform duration-300 ease-in-out"
+                onClick={handlePlayPause}
+              >
+                <svg
+                  width="40"
+                  height="40"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      cx="20"
-                      cy="20"
-                      r="18.5"
-                      stroke="white"
-                      strokeWidth="3"
-                    />
-                    <path
-                      d="M25.3333 20.0001L16 14.6667M16 14.6667V25.3334V14.6667ZM25.3333 20.0001L16 25.3334L25.3333 20.0001Z"
-                      stroke="white"
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
-                {videos?.[7] && process.env.NEXT_PUBLIC_API_BASE_URL && (
-                  <video
-                    className="w-full rounded-sm"
-                    ref={ref}
-                    src={process.env.NEXT_PUBLIC_API_BASE_URL + videos?.[7].url}
-                    onPlay={handlePlay}
-                    onPause={handlePause}
+                  <circle
+                    cx="20"
+                    cy="20"
+                    r="18.5"
+                    stroke="white"
+                    strokeWidth="3"
                   />
-                )}
-              </div>
-              <div className="flex w-full flex-col items-start gap-y-4 text-white md:w-1/2">
-                <h3 className="text-[32px] font-semibold">
-                  {videos?.[7].title}
-                </h3>
-                <p className="text-[24px]">{videos?.[7].description}</p>
-                <button className="rounded-md border border-legendary-500 bg-transparent px-4 py-2 text-[20px] font-medium text-legendary-500">
-                  View More
-                </button>
-              </div>
+                  <path
+                    d="M25.3333 20.0001L16 14.6667M16 14.6667V25.3334V14.6667ZM25.3333 20.0001L16 25.3334L25.3333 20.0001Z"
+                    stroke="white"
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+              {videos?.[7] && process.env.NEXT_PUBLIC_API_BASE_URL && (
+                <video
+                  className="w-full rounded-sm"
+                  ref={ref}
+                  src={process.env.NEXT_PUBLIC_API_BASE_URL + videos?.[7].url}
+                  onPlay={handlePlay}
+                  onPause={handlePause}
+                />
+              )}
             </div>
-          </MciContainer>
-        </section>
-      </main>
-    </StripeWrapper>
+            <div className="flex w-full flex-col items-start gap-y-4 text-white md:w-1/2">
+              <h3 className="text-[32px] font-semibold">{videos?.[7].title}</h3>
+              <p className="text-[24px]">{videos?.[7].description}</p>
+              <button className="rounded-md border border-legendary-500 bg-transparent px-4 py-2 text-[20px] font-medium text-legendary-500">
+                View More
+              </button>
+            </div>
+          </div>
+        </MciContainer>
+      </section>
+    </main>
   );
 }
--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -7,8 +7,6 @@ import CustomControls from './components/CustomControls';
 import { useVoteContext } from '@/lib/context/vote context';
 import CArdsFilterView from '../components/CardsFilterView';
 import PlayVideo from '../components/PlayVideo';
-import StripeWrapper from '../components/StripeWrapper';
-import PaymentForm from '../components/PaymentForm';
 
 function Vote() {
   const { data: videos } = useGetListVideos();
@@ -38,121 +36,117 @@ function Vote() {
     );
 
   return (
-    <StripeWrapper>
-      <div className={`relative ${isVoteOpen && 'h-screen overflow-hidden'}`}>
-        <PaymentForm />
-
-        {isVoteOpen && (
-          <>
-            <div
-              className="absolute bottom-0 left-0 right-0 top-0 z-20 bg-black/20 backdrop-blur-sm transition-transform duration-1000 ease-in-out hover:cursor-pointer"
-              onClick={() => setIsVoteOpen(false)}
-            />
-          </>
-        )}
-
-        {displayVid && (
-          <PlayVideo video={displayVid} setDisplayVid={setDisplayVid} />
-        )}
-
-        {/* top videos seciton */}
-        <section className="relative h-[580px]">
-          {!isVideoPlaying && (
-            <div className="absolute bottom-0 end-0 start-0 top-0 z-0 bg-black/70" />
-          )}
-          <video
-            ref={videoRef}
-            className="absolute h-full w-full object-cover"
-            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}
-            onEnded={() => setIsVideoPlaying(false)}
-            controlsList="nodownload"
-            controls={false}
-            disablePictureInPicture
-            disableRemotePlayback
-            preload="metadata"
+    <div className={`relative ${isVoteOpen && 'h-screen overflow-hidden'}`}>
+      {isVoteOpen && (
+        <>
+          <div
+            className="absolute bottom-0 left-0 right-0 top-0 z-20 bg-black/20 backdrop-blur-sm transition-transform duration-1000 ease-in-out hover:cursor-pointer"
+            onClick={() => setIsVoteOpen(false)}
           />
-          <div className="ms-[160px] flex h-full flex-row justify-start">
-            <div className="start-[150px] top-0 flex h-full flex-col items-start gap-y-4 overflow-y-auto text-white">
-              <div className="z-10 border-e border-white pe-10">
-                <h1 className="z-10">Videos</h1>
-                {videos?.map((video: videoType, i: number) => {
-                  return (
-                    <div key={`${video.title} - ${i}`} className="mb-4">
-                      <video
-                        className="w-[270px] rounded-sm"
-                        src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
-                      />
-                      <div className="flex flex-row items-center justify-between">
-                        <div className="flex flex-row items-center justify-start gap-x-4">
-                          <div className="relative h-10 w-10 overflow-clip rounded-full">
-                            <Image
-                              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
-                              fill
-                              alt="singer image"
-                              loading="lazy"
-                            />
-                          </div>
-                          <div className="">
-                            <p>{video.title}</p>
-                            <p>{`By ${video.artist.name}`}</p>
-                          </div>
+        </>
+      )}
+
+      {displayVid && (
+        <PlayVideo video={displayVid} setDisplayVid={setDisplayVid} />
+      )}
+
+      {/* top videos seciton */}
+      <section className="relative h-[580px]">
+        {!isVideoPlaying && (
+          <div className="absolute bottom-0 end-0 start-0 top-0 z-0 bg-black/70" />
+        )}
+        <video
+          ref={videoRef}
+          className="absolute h-full w-full object-cover"
+          src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${videos?.[0].url}`}
+          onEnded={() => setIsVideoPlaying(false)}
+          controlsList="nodownload"
+          controls={false}
+          disablePictureInPicture
+          disableRemotePlayback
+          preload="metadata"
+        />
+        <div className="ms-[160px] flex h-full flex-row justify-start">
+          <div className="start-[150px] top-0 flex h-full flex-col items-start gap-y-4 overflow-y-auto text-white">
+            <div className="z-10 border-e border-white pe-10">
+              <h1 className="z-10">Videos</h1>
+              {videos?.map((video: videoType, i: number) => {
+                return (
+                  <div key={`${video.title} - ${i}`} className="mb-4">
+                    <video
+                      className="w-[270px] rounded-sm"
+                      src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.url}`}
+                    />
+                    <div className="flex flex-row items-center justify-between">
+                      <div className="flex flex-row items-center justify-start gap-x-4">
+                        <div className="relative h-10 w-10 overflow-clip rounded-full">
+                          <Image
+                            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${video.artist.avatar.url}`}
+                            fill
+                            alt="singer image"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="">
+                          <p>{video.title}</p>
+                          <p>{`By ${video.artist.name}`}</p>
                         </div>
                       </div>
                     </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div
-              className={`z-10 flex flex-col justify-end pb-40 ps-10 text-white ${
-                isVideoPlaying && 'w-full'
-              }`}
-            >
-              {!isVideoPlaying ? (
-                <>
-                  <h1 className="text-[72px] font-bold capitalize text-transparent [-webkit-text-stroke:2px_white]">
-                    {videos?.[0].title}
-                  </h1>
-                  <p className="mb-6 text-[40px] font-medium underline">
-                    by {videos?.[0].artist.name}
-                  </p>
-                  <button
-                    className="flex w-fit flex-shrink flex-row items-center justify-between gap-x-4 rounded-full bg-legendary-500 px-6 py-3 text-white"
-                    onClick={playVideo}
-                  >
-                    <p className="text-[24px]">Play Video</p>
-                    <span>
-                      <svg
-                        width="15"
-                        height="20"
-                        viewBox="0 0 15 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M0.742348 0.154431C0.00534203 0.584573 -0.000601596 0.664177 2.40459e-05 10.0454C0.000493277 18.5391 0.0120676 18.8864 0.307996 19.3726C0.692609 20.0048 1.2438 20.1547 2.03649 19.8427C2.92584 19.4927 14.3141 11.6407 14.6277 11.1612C14.7741 10.9373 14.9297 10.4866 14.9735 10.1594C15.1237 9.03856 14.8406 8.78815 8.5283 4.45946C5.31063 2.25296 2.42955 0.343412 2.1258 0.216328C1.4842 -0.0522569 1.12415 -0.0682403 0.742348 0.154431Z"
-                          fill="white"
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </>
-              ) : (
-                <CustomControls
-                  currentVideo={videoRef.current}
-                  isVideoPlaying={isVideoPlaying}
-                  setIsVideoPlaying={setIsVideoPlaying}
-                />
-              )}
+                  </div>
+                );
+              })}
             </div>
           </div>
-        </section>
-        {/* filters and videos section */}
-        <CArdsFilterView showFilters={true} setDisplayVid={setDisplayVid} />
-      </div>
-    </StripeWrapper>
+          <div
+            className={`z-10 flex flex-col justify-end pb-40 ps-10 text-white ${
+              isVideoPlaying && 'w-full'
+            }`}
+          >
+            {!isVideoPlaying ? (
+              <>
+                <h1 className="text-[72px] font-bold capitalize text-transparent [-webkit-text-stroke:2px_white]">
+                  {videos?.[0].title}
+                </h1>
+                <p className="mb-6 text-[40px] font-medium underline">
+                  by {videos?.[0].artist.name}
+                </p>
+                <button
+                  className="flex w-fit flex-shrink flex-row items-center justify-between gap-x-4 rounded-full bg-legendary-500 px-6 py-3 text-white"
+                  onClick={playVideo}
+                >
+                  <p className="text-[24px]">Play Video</p>
+                  <span>
+                    <svg
+                      width="15"
+                      height="20"
+                      viewBox="0 0 15 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M0.742348 0.154431C0.00534203 0.584573 -0.000601596 0.664177 2.40459e-05 10.0454C0.000493277 18.5391 0.0120676 18.8864 0.307996 19.3726C0.692609 20.0048 1.2438 20.1547 2.03649 19.8427C2.92584 19.4927 14.3141 11.6407 14.6277 11.1612C14.7741 10.9373 14.9297 10.4866 14.9735 10.1594C15.1237 9.03856 14.8406 8.78815 8.5283 4.45946C5.31063 2.25296 2.42955 0.343412 2.1258 0.216328C1.4842 -0.0522569 1.12415 -0.0682403 0.742348 0.154431Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </span>
+                </button>
+              </>
+            ) : (
+              <CustomControls
+                currentVideo={videoRef.current}
+                isVideoPlaying={isVideoPlaying}
+                setIsVideoPlaying={setIsVideoPlaying}
+              />
+            )}
+          </div>
+        </div>
+      </section>
+      {/* filters and videos section */}
+      <CArdsFilterView showFilters={true} setDisplayVid={setDisplayVid} />
+    </div>
   );
 }
 
